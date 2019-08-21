110670d3b57c0cd1bc47d1abbe7e27c54830f892
Ronan-Ramdas <ronan4d97@gmail.com>
2019-08-21 14:18:47 +0530

    Hello World Application

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int i = 45;
+        String string = "Hello Java from Area ";
+        System.out.println(string + (6 + i));
+    }
+}
