bf3a13027a80c3606fbcc98ec42d8d2617f4b250
Ronan-Ramdas <ronan4d97@gmail.com>
2019-08-21 15:14:57 +0530

    Added more Calculator functions

--- src/Hello.java
+++ src/Hello.java
@@ -3,6 +3,8 @@ public class Hello {
         int a = 5, b = 6;
         System.out.println(a+b);
         System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
         System.out.println("Calculator");
     }
 }
