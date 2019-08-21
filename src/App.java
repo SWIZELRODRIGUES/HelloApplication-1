be6fb2b4d7504ab29e688cbff6b01ab22a021aa8
Ronan-Ramdas <ronan4d97@gmail.com>
2019-08-21 14:26:44 +0530

    Aliens in Area 51

--- src/App.java
+++ src/App.java
@@ -3,5 +3,6 @@ public class App {
         int i = 45;
         String string = "Hello Java from Area ";
         System.out.println(string + (6 + i));
+        System.out.println("Aliens in Area 51");
     }
 }
